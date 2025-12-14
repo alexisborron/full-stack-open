7543f7643aa174a65040c75d76a621b679283125
alexisborron <188102818+alexisborron@users.noreply.github.com>
2025-12-14 15:45:37 -0600

    Add frontend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
 
 app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(express.static('dist'))
 app.use(express.json())
 
 let notes = [
